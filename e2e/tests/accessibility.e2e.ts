1fa49fe027582c4d906537d7e9138c80a7e291e7
“Agustin <agustin.cammarotamuti.contractor@gdevtools.com>
2024-07-27 17:22:19 -0300

    feat(accessibility.e2e.ts, ng-update.yml, audit-dependencies.yml, package.json, build.yml): fix code [GH0039]

--- e2e/tests/accessibility.e2e.ts
+++ e2e/tests/accessibility.e2e.ts
@@ -1,14 +1,18 @@
 
 import { setupBrowserHooks, getBrowserState } from './utils';
+import { AxePuppeteer } from '@axe-core/puppeteer';
 
-// Disabled because it can only be run locally
-xdescribe('Accessibility', () => {
+
+describe('Accessibility', () => {
   setupBrowserHooks();
   
   it('should generate a accessibility report', async () => {
     const { page } = getBrowserState();
-    const snapshot = await page.accessibility.snapshot();
-    console.log(snapshot);
-    expect(snapshot).not.toBeNull();
+    await page.setBypassCSP(true);
+    await page.reload();
+
+    const result = await new AxePuppeteer(page).analyze();
+    console.log(result.violations);
+    expect(result.violations.length).toBeLessThan(2);
   }, 50000);
 });
